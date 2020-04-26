19d6a9c1323251cc732206d9851e242a6c058425
ManyMeanings <manymeanings@outlook.com>
2020-04-26 12:08:51 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: ['/images/test.jpg','/images/test.jpg','/images/test.jpg'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500
+    
+  },
+  onLoad: function () {
+    wx.setNavigationBarTitle({
+      title: "今日好券"
+    })
+    
+  },
+  
+})
