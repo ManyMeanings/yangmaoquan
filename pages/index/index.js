38c1483b83b578f838ca449696677b33b7e594bb
Rick Tao <manymeaningsoftheworld@gmail.com>
2020-05-13 10:40:31 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -10,11 +10,9 @@ Page({
     autoplay: true,
     interval: 2000,
     duration: 500,
-    product_array:""
+    product_array: "",
   },
   onLoad: function () {
-    var startTime = new Date();
-    console.log("time"+startTime);
     wx.setNavigationBarTitle({
       title: "今日好券"
     })
@@ -40,8 +38,7 @@ Page({
         console.log(res.data)
         that.setData({
           product_array: res.data						//设置数据，将表中查询出来的信息传给info
-        })
-        console.log(new Date()-startTime);     
+        }) 
       },
       fail: function(err) {
         console.log(err)
@@ -55,10 +52,32 @@ Page({
     })
   
   },
+  onReachBottom: function () {
+    var that =this;
+    wx.request({
+      url: 'https://tp.adplay.ink/QueryProductByRandom.php',
+      data: {},									//传参
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function(res) {
+        console.log(res.data)
+        that.setData({
+          product_array: that.data.product_array.concat(res.data)
+        })
+      },
+      fail: function(err) {
+        console.log(err)
+      }
+    })
+    
+ },
   click: function (option) {
     wx.navigateTo({
       url: '/pages/products/products?id=' + option.currentTarget.dataset.id + '&name=' + option.currentTarget.dataset.name + '&img=' + option.currentTarget.dataset.img + '&pre=' + option.currentTarget.dataset.pre + '&price=' + option.currentTarget.dataset.price + '&category=' + option.currentTarget.dataset.category + '&desc=' + option.currentTarget.dataset.desc,
     })
   },
+  
+
   
 })
