e2129bada0997ada4b26fee3c5aa5a3bcb3549b3
Rick Tao <manymeaningsoftheworld@gmail.com>
2020-05-26 10:02:58 +0800

    fix

--- pages/search/search.js
+++ pages/search/search.js
@@ -2,16 +2,15 @@
 var WxSearch = require('../../wxSearch/wxSearch.js');
 var app = getApp();
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
     display: "none",
-    RearchResultList:"",
-    url:"",
-    timeAndPrice:"",
-    Key:""
+    RearchResultList: "",
+    url: "",
+    timeAndPrice: "",
+    Key: ""
   },
   /**
    * 生命周期函数--监听页面加载
@@ -19,38 +18,51 @@ Page({
   onLoad: function (options) {
     var that = this;
     //初始化的时候渲染wxSearchdata
-    WxSearch.init(that, 43, ['自定义', '的热门', '搜索', '列表']);
-    WxSearch.initMindKeys(['检索', '内容', '的提示']);
-    console.log(app.globalData.key)
+    wx.request({
+      url: 'https://tp.adplay.ink/QueryHotSearch.php',
+      success: function (res) {
+        app.globalData.hot_list = res.data;
+        console.log(app.globalData.hot_list);
+        WxSearch.init(that, 43, app.globalData.hot_list);
+        WxSearch.initMindKeys(['检索', '内容', '的提示']);
+      }
+    })
   },
   wxSearchInput: function (e) {
     var that = this
     WxSearch.wxSearchInput(e, that);
     app.globalData.key = e.detail.value
-
-    console.log(this.data.Key)
+    console.log(app.globalData.Key)
   },
   wxSearchFn: function (e) {
     var that = this
     WxSearch.wxSearchAddHisKey(that);
-    console.log(this.data.Key)
+    console.log(app.globalData.key)
     wx.request({
-      url: 'https://tp.adplay.ink/GETURL.php',
-      data:{
-        keyword:app.globalData.key
-      },
-      success:function(res)
-      {
-        that.setData({
-          RearchResultList:res.data,
-        })
-        console.log(that.data.url),
-        console.log(res.data)
-      },
-      fail:function(res){
-        console.log(res.data)
-      }
-    })
+        url: 'https://tp.adplay.ink/GETURL.php',
+        data: {
+          keyword: app.globalData.key
+        },
+        success: function (res) {
+          that.setData({
+            RearchResultList: res.data,
+          })
+          console.log(that.data.url),
+            console.log(res.data)
+        },
+        fail: function (res) {
+          console.log(res.data)
+        }
+      }),
+      wx.request({
+        url: 'https://tp.adplay.ink/RecordQuery.php',
+        data: {
+          product_id: app.globalData.key
+        },
+        success: function (res) {
+
+        }
+      })
   },
   wxSerchFocus: function (e) {
     var that = this
@@ -76,9 +88,9 @@ Page({
     var that = this
     WxSearch.wxSearchHiddenPancel(that);
   },
-  click: function(option){
+  click: function (option) {
     wx.navigateTo({
-      url: '/pages/data/data?name='+ option.currentTarget.dataset.name + '&price=' + option.currentTarget.dataset.price + '&img=' + option.currentTarget.dataset.img + '&source=' + option.currentTarget.dataset.source + '&url=' + option.currentTarget.dataset.url, 
+      url: '/pages/data/data?name=' + option.currentTarget.dataset.name + '&price=' + option.currentTarget.dataset.price + '&img=' + option.currentTarget.dataset.img + '&source=' + option.currentTarget.dataset.source + '&url=' + option.currentTarget.dataset.url,
     })
   },
 
