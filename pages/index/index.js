2dad34c448f91e3cb4319140b767941945d29744
Rick Tao <manymeaningsoftheworld@gmail.com>
2020-06-03 11:23:05 +0800

    update pages/history & pages/data

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    imgUrls: ['/images/test.jpg','/images/test.jpg','/images/test.jpg'],
+    imgUrls: ['/images/test.jpg', '/images/test.jpg', '/images/test.jpg'],
     indicatorDots: true,
     vertical: false,
     autoplay: true,
@@ -17,61 +17,60 @@ Page({
     wx.request({
       url: 'https://tp.adplay.ink/QueryProductByClientId.php',
       data: {
-        'client_id':wx.getStorageSync('openId'),
-      },									//传参
-      success: function(res) {
+        'client_id': wx.getStorageSync('openId'),
+      }, //传参
+      success: function (res) {
         that.setData({
-          user_product_array: res.data						//设置数据，将表中查询出来的信息传给info
+          user_product_array: res.data //设置数据，将表中查询出来的信息传给info
         })
-        if(res.data=='0')      
-      { 
-      wx.request({
-      url: 'https://tp.adplay.ink/QueryProductByRandom.php',
-      data: {},									//传参
-      header: {
-        'content-type': 'application/json'
-      },
-      success: function(res) {
-        console.log(res.data)
-        that.setData({
-          product_array: res.data						//设置数据，将表中查询出来的信息传给info
-        }) 
-      },
-      fail: function(err) {
-        console.log(err)
-      }
-    })
+        if (res.data == '0') {
+          wx.request({
+            url: 'https://tp.adplay.ink/QueryProductByRandom.php',
+            data: {}, //传参
+            header: {
+              'content-type': 'application/json'
+            },
+            success: function (res) {
+              console.log(res.data)
+              that.setData({
+                product_array: res.data //设置数据，将表中查询出来的信息传给info
+              })
+            },
+            fail: function (err) {
+              console.log(err)
+            }
+          })
         }
       },
-      fail: function(err) {
+      fail: function (err) {
         console.log(err)
       }
     })
-  
+
   },
   onReachBottom: function () {
-    var that =this;
+    var that = this;
     wx.request({
       url: 'https://tp.adplay.ink/QueryProductByRandom.php',
-      data: {},									//传参
+      data: {}, //传参
       header: {
         'content-type': 'application/json'
       },
-      success: function(res) {
+      success: function (res) {
         console.log(res.data)
         that.setData({
           product_array: that.data.product_array.concat(res.data)
         })
       },
-      fail: function(err) {
+      fail: function (err) {
         console.log(err)
       }
     })
-    
- },
+
+  },
   click: function (option) {
     var url = option.currentTarget.dataset.url;
-    var encodeURIComponent_url = encodeURIComponent (url);
+    var encodeURIComponent_url = encodeURIComponent(url);
     wx.navigateTo({
       url: '/pages/products/products?id=' + option.currentTarget.dataset.id + '&name=' + option.currentTarget.dataset.name + '&img=' + option.currentTarget.dataset.img + '&pre=' + option.currentTarget.dataset.pre + '&price=' + option.currentTarget.dataset.price + '&category=' + option.currentTarget.dataset.category + '&desc=' + option.currentTarget.dataset.desc + '&url=' + encodeURIComponent_url,
     })
@@ -81,12 +80,17 @@ Page({
       url: '/pages/search/search'
     })
   },
-  rank:function(option){
+  rank: function (option) {
     wx.navigateTo({
       url: '/pages/rank/rank',
     })
+  },
+  history: function (option) {
+    wx.navigateTo({
+      url: '/pages/history/history',
+    })
   }
-  
 
-  
+
+
 })
